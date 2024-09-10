0a22f0d9d64bf96f22e6aa1708b6380f7a2a2a16
Guy Oliver <OlivertwstTech@gmail.com>
2024-09-10 14:16:25 -0700

    finished profile page

--- front/src/pages/Profile.jsx
+++ front/src/pages/Profile.jsx
@@ -1,43 +1,42 @@
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { getAuth, deleteUser } from "react";
-// import { get } from "../../back/app";
-
-
 
 function Profile() {
-
-    const [auth, getAuth] = useState();
-    const [user, authCurrentUser] = useState();
     const navigate = useNavigate();
-    const [errorMessage, setError] = useState('')
-    const [userId, setUserId] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [profilePicUrl, setProfilePicUrl] = useState(null); // Store the URL of the profile picture displayed in the UI
-    const [profilePicFile, setProfilePicFile] = useState(null); // Stores selected image file when a user chooses to update their profile picture
-
+    const [errorMessage, setErrorMessage] = useState('')
 
     const handleSubmit = e => {
         // Prevent the default submit and page reload
         e.preventDefault()
-        const auth = localStorage.getItem('auth')
-
+        const auth = JSON.parse(localStorage.getItem('auth'));
+        const config = {
+            headers: {
+                Authorization: `Bearer ${auth.token}`
+            }
+        };
         // Handle validations
-        
+
         axios
-            .delete(`http://localhost:3000/api/auth/${auth.userId}`, {})
+            .delete(`http://localhost:3000/api/auth/users/${auth.userId}`, config)
             .then(response => {
                 // Handle response
                 localStorage.clear();
                 navigate('/signup');
             }).catch(error => {
-                setError('Invalid Username and/or Password')
+                setErrorMessage('Invalid Username and/or Password')
             });
     }
-   
-    return <div>Welcome to your profile...</div>;
+
+    return (
+        <div className='deleteUser'>
+            <h2>Delete</h2>
+            <div>
+                <button onClick={handleSubmit}>Delete Profile</button>
+            </div>
+        </div>
+        // <div>Welcome to your profile...</div>;
+    );
 }
 // TODO ADD A BUTTON FOR THE USER TO DELETE THEIR ACCT
 
